f131ece2f0e840c2413827bf7b85292c63f2472f
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-14 01:00:55 -0400

    updated loop from what didn't push esarlier,also updated quickSelect to pdate pivot variable

--- QuickSort.java
+++ QuickSort.java
@@ -26,7 +26,7 @@ public class QuickSort {
     //loop through ints from start to end
     int i1 = start+1; // last index of a number smaller than pivot.
     int i2 = end;
-    while (i2 > i1) {
+    while (i2 >= i1) {
     //  System.out.println(i1+", "+i2);
     //  System.out.println(Arrays.toString(data));
 
@@ -39,9 +39,11 @@ public class QuickSort {
         i2--;
       }
       //if data[i2] is proper, increment i2
+      /*
       if (data[i2] >= pivot) {
         i2--;
       }
+      */
       //if data[i1] is proper increment it.
       if (data[i1] < pivot) {
         i1++;
@@ -52,12 +54,24 @@ public class QuickSort {
     //System.out.println("pivot: "+i2);
 
     //now loop through data to place pivot.
+    for (int i=start;i<=end;i++) {
+      if (pivot > data[i]) {
+        int temp = data[i-1];
+        data[i-1] = pivot;
+        data[start] = temp;
+        i = end+1; // break out of loop.
+        return i-1;
+      }
+    }
+    /*
     int temp = data[i2-1]; // data[i2] is the last value smaller than pivot. swap it with pivot.
     data[start] = temp;
     data[i2-1] = pivot;
+    */
 
     System.out.println("value: "+pivot);
-    System.out.println("index: "+i2);System.out.println(Arrays.toString(data)+"\n");
+    System.out.println("index: "+i2);
+    System.out.println(Arrays.toString(data)+"\n");
     return i2-1;
   }
 
@@ -66,14 +80,21 @@ public class QuickSort {
   public static int quickSelect(int[] data, int k){
     int pivot = partition(data,0,data.length-1);
     for (int i=0;i<data.length;i++) {
+      int temp = pivot;
       System.out.println("pivot: "+pivot);
       System.out.println(Arrays.toString(data));
 
       if (pivot == k) return data[pivot];
 
-      if (pivot < k) partition(data,pivot+1,data.length-1);
+      if (pivot < k) {
+        System.out.println("checking between "+(temp+1)+", "+(data.length-1));
+        pivot = partition(data,temp+1,data.length-1);
+      }
 
-      if (pivot > k) partition(data,0,pivot-1);
+      if (pivot > k) {
+        System.out.println("checking between 0, "+(temp-1));
+        pivot = partition(data,0,temp-1);
+      }
     }
     return -1;
   }
@@ -82,7 +103,7 @@ public class QuickSort {
     int[] data = {100,99,98,4,2,7};
     //for (int i=0;i<10;i++) {
     for (int i=0;i<10;i++)
-      partition(data,0,5);
+      quickSelect(data,3);
 
     //}
   }
